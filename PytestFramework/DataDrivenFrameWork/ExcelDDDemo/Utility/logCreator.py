9a20285b7430d77dbe3133b275f052852d87752c
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-06-09 16:56:09 +0530

    Updated logCreator

--- PytestFramework/DataDrivenFrameWork/ExcelDDDemo/Utility/logCreator.py
+++ PytestFramework/DataDrivenFrameWork/ExcelDDDemo/Utility/logCreator.py
@@ -1,14 +1,20 @@
 import logging
+import sys
 
 
 def log_generator():
 
     logging.basicConfig(
-        filename="testlogreport.log",
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S %p",
+        handlers=[
+            logging.FileHandler("testlogreport.log", mode="a"),
+            logging.StreamHandler(sys.stdout),
+        ],
         force=True,
     )
 
-    return logging.getLogger()
+    logger = logging.getLogger()
+
+    return logger
