880fa40cfc71df5ac3bfa8190e469d73cfe89718
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-06-10 15:49:10 +0530

    Update configReader.py

--- PytestFramework/PageObjectModel/POM_1/Utilities/configReader.py
+++ PytestFramework/PageObjectModel/POM_1/Utilities/configReader.py
@@ -4,6 +4,6 @@ from configparser import ConfigParser
 def get_Config_Data(c, k):
     config = ConfigParser()
     config.read(
-        "D:\PythonSeleniumExpleo\PytestFramework\PageObjectModel\POM_1\Configuration\config.ini"
+        ""
     )
     return config.get(c, k)
