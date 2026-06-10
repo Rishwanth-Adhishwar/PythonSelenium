81d6bb2bb2a7ecb5de42843f968b3bd5dd19dad2
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-06-10 15:49:34 +0530

    Update configReader.py

--- PytestFramework/PageObjectModel/POM_1/Utilities/configReader.py
+++ PytestFramework/PageObjectModel/POM_1/Utilities/configReader.py
@@ -4,6 +4,6 @@ from configparser import ConfigParser
 def get_Config_Data(c, k):
     config = ConfigParser()
     config.read(
-        ""
+        "PytestFramework/PageObjectModel/POM_1/Configuration/config.ini"
     )
     return config.get(c, k)
